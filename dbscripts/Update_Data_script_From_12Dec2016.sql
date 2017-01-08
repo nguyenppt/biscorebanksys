e0ae64cf8984f9e5dad4d7dd5dc42a1b98536147
NghiaLe-PC\NghiaLe <usr.nghiale@gmail.com>
2017-01-08 19:25:00 +0700

    BIS-FT-3 Phiếu chuyển khoản Chính tả, nôi dung đồng nhất với các phiếu.  NOT YET "[BIS-8/Jan/2017] fixed +++++++++++++++++++ Chữ Mỹ bị thiếu dấu ngã. Đảm bảo viết code đúng cho các đơn vị tiền tệ khác. Vì không thể test hết các đơn vị tiền tệ khác xem có đúng chính tả hay không" Fixed

--- dbscripts/Update_Data_script_From_12Dec2016.sql
+++ dbscripts/Update_Data_script_From_12Dec2016.sql
@@ -1,9 +1,17 @@
 ﻿/***
 ---------------------------------------------------------------------------------
 -- 11 Dec 2016 : Nghia : viet nam currency
+-- 8  Jan 2017 : Nghia : viet nam currency - My
 ---------------------------------------------------------------------------------
 ***/
 Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'do la', N' đô la');
 Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'dong', N'đồng');
 Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Viet Nam', N' Việt Nam');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'My', N'Mỹ');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Uc', N'Úc');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Nhan Dan Te', N'Nhân Dân Tệ');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Bang Anh', N'Bảng Anh');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Vang', N'Vàng');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Hong Kong', N'Hồng Kông');
+Update bcurrency set [vietnamese] = REPLACE([vietnamese], 'Yen Nhat', N'Yên Nhật');
 Update bcurrency set [Pence] = REPLACE([Pence], 'dong', N'đồng');
